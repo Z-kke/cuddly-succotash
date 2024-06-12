4dc5c462a197af7cefbfb87f27156d03064b151f
Name <sakurho@gmail.com>
2024-06-12 23:16:30 +0300

    Added some lorem ipsum to the landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-white text-6xl font-bold transform transition-all duration-1000 ease-in-out translate-x-full animate-slide-in">
-        Landing Page
-      </h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="mt-20">
+        <h1 className="text-white text-6xl font-bold transform transition-all duration-1000 ease-in-out -translate-x-full animate-slide-in">
+          Landing Page
+        </h1>
+        <p className="mt-8 text-white text-2xl transform transition-all duration-1000 ease-in-out -translate-x-full animate-slide-in">
+          Incididunt dolore aute esse elit nulla consectetur anim. Nostrud
+          adipisicing cupidatat esse incididunt. Nostrud ad labore reprehenderit
+          excepteur duis est sunt. Magna officia deserunt cillum cupidatat.
+          Consequat minim do occaecat ipsum commodo nisi consequat non. Aute qui
+          qui sunt duis ullamco nulla enim non reprehenderit. Deserunt irure
+          occaecat mollit ad officia ullamco laboris commodo enim. Nisi amet
+          velit aliquip quis dolore. Ex velit consequat enim magna cillum
+          nostrud deserunt cillum elit deserunt est eu. Ad excepteur
+          reprehenderit aute aliquip. Velit aliqua mollit ex mollit quis. Id do
+          velit quis culpa. Veniam Lorem elit duis reprehenderit exercitation
+          esse occaecat cillum sint aute elit. Ut nisi reprehenderit quis irure
+          laborum pariatur est pariatur occaecat nulla. Excepteur dolor sunt
+          deserunt eiusmod cupidatat fugiat. Sit cillum quis deserunt
+          consectetur velit mollit cupidatat eiusmod et.
+          <br />
+          <br />
+          Nisi pariatur voluptate exercitation consequat Lorem excepteur anim
+          elit. Aliquip elit dolor cillum pariatur tempor nisi excepteur.
+          Deserunt id pariatur laborum deserunt ea esse do consequat irure
+          ullamco anim. Ex officia enim dolor enim ea ullamco nisi elit laborum
+          voluptate culpa voluptate mollit nostrud. Ea commodo culpa
+          reprehenderit aliquip cupidatat do aliqua sunt laboris ut sit dolor.
+          Reprehenderit cupidatat sint sunt id sit excepteur. Ipsum aliqua
+          mollit sunt ut aliquip duis. Labore amet amet magna quis ea pariatur
+          non.
+        </p>
+      </div>
+      <div className="flex-grow" />
     </main>
   );
 }
