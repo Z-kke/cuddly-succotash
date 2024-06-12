a70f341b36f4a29d1d7bee0042cb4cb7623c025d
Name <sakurho@gmail.com>
2024-06-12 23:06:36 +0300

    Add navbar.

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,37 @@
+import { AppBar, Toolbar } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "black" }}>
+      <Toolbar className="flex justify-between">
+        <Link href="/" passHref>
+          <Image
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className="dark:invert"
+            width={100}
+            height={24}
+            priority
+          />
+        </Link>
+        <div>
+          <Link href="/about" passHref>
+            <p className="text-white inline-block mr-4 text-2xl font-semibold">
+              About
+            </p>
+          </Link>
+          <Link href="/contact" passHref>
+            <p className="text-white inline-block mr-4 text-2xl font-semibold">
+              Contact
+            </p>
+          </Link>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
