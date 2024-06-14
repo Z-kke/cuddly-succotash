8c54d903e02907fe548c3b70114f5e9b8ddf282d
Personal github <sakurho@gmail.com>
2024-06-14 12:35:29 +0300

    Smooth out the font.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} antialiased`}>
         <AppRouterCacheProvider>
           <Navbar />
           <main className="pt-20">{children}</main>
